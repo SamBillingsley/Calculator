ce39a0d2e75c761ffa7b3a38c92140e06b7b9671
SamBillingsley <sambillingsleywork@gmail.com>
2023-05-16 12:26:23 -0500

    Add basic layout to calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+
+const addition = document.querySelector("#addition");
+const subtraction = document.querySelector("#subtraction");
+const multiplication = document.querySelector("#multiplication");
+const division = document.querySelector("#division");
+
+let equation = [];
+
+one.addEventListener("click", () => equation << 1);
